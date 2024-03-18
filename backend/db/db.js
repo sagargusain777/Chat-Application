04f7e0027619bfdf0639be6482d52526b54c9c68
uwuxverticuse <sagargusain150@gmail.com>
2024-03-18 23:01:13 +0530

    ✅  Added Connection with MongoDB

---
+++ backend/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const DB_name = "Chat-Application"
+
+const connectDB = async ()=>{
+
+    try {
+        const connectionInstance = await mongoose.connect(`process.env.MONGO_URI/${DB_name}`)
+         console.log(`Database Connection Successful : ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.error(`Database Connection Failed : ${error.message}`);
+        process.exit(1);
+    }
+}
