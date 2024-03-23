dfae5fba189eb7049975b08ca8531217a9cd3dd9
uwuxverticuse <sagargusain150@gmail.com>
2024-03-24 02:27:29 +0530

    📝 Connection with  Database

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const DB_name = "Chat-Application"
 
-const connectDB = async ()=>{
+const connectDB = async()=>{
 
     try {
-        const connectionInstance = await mongoose.connect(`process.env.MONGO_URI/${DB_name}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_name}`)
          console.log(`Database Connection Successful : ${connectionInstance.connection.host}`);
         
     } catch (error) {
@@ -13,3 +13,5 @@ const connectDB = async ()=>{
         process.exit(1);
     }
 }
+
+export default connectDB;
