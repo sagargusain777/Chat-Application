2a1446e6574acac428202b44a3d421b01389c92d
uwuxverticuse <sagargusain150@gmail.com>
2024-04-05 14:49:00 +0530

    📝 Logout Controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,101 +1,100 @@
-import User from "../models/user.models.js"
-import bcrypt from "bcryptjs"
+import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
 import generateTokenandsetCookies from "../utils/generateToken.js";
 
-export const  signup= async(req,res)=>{
-  try{
-    const {fullName,username,password,confirmpassword,gender} =req.body;
-    
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmpassword, gender } = req.body;
+
     //Checking that the password and confirm password are same or not
-    if(!(password===confirmpassword)){
+    if (!(password === confirmpassword)) {
       console.log("Password does not match");
-      return res.status(400).json({message:"Password does not match"})
-    }
-   
-    //Checking the user from the username that it already exists or not
-    const user = await User.findOne({username});
-    if(user){
-      console.log("User already exists");
-      return res.status(400).json({message:"User already exists"})
+      return res.status(400).json({ message: "Password does not match" });
     }
 
+    //Checking the user from the username that it already exists or not
+    const user = await User.findOne({ username });
+    if (user) {
+      console.log("User already exists");
+      return res.status(400).json({ message: "User already exists" });
+    }
 
     //Profile image based on gender
-    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${fullName}`
-    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${fullName}`
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${fullName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${fullName}`;
 
     //Hashing the password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     //Creating the user in the database
     const newUser = new User({
       fullName,
       username,
-      password : hashedPassword,
+      password: hashedPassword,
       gender,
-      profilePic :(gender ==="male")?boyProfilePic:girlProfilePic
-      
-    })
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
 
-    if(newUser){
+    if (newUser) {
+      generateTokenandsetCookies(newUser._id, res);
 
       await newUser.save();
+
       console.log("User created");
-      return res.status(201).json(
-        {
-          _id:newUser._id,
-          fullName: newUser.fullName,
-          username: newUser.username,
-          gender:newUser.gender
-        }
-      )
-
-    }
-    else{
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        gender: newUser.gender,
+      });
+    } else {
       console.log("User not created");
-      return res.status(400).json({message:"User not created"})
+      return res.status(400).json({ message: "User not created" });
     }
-    
-
-
-  }catch(error){
+  } catch (error) {
     console.log(` Error in SignUp controller: ${error}`);
-    res.status(500).json({message:"Internal Server Error"})
+    res.status(500).json({ message: "Internal Server Error" });
   }
-}
-export const  login = async(req,res)=>{
-    try {
-      const {username,password} = req.body;
-      
-      //Checking the user from the database
-      const user = await User.findOne({username: username});
-      
-      //Checking that the password matches with the database password or not
-      const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
-      if(!user || !isPasswordCorrect){
-        console.log("Invalid Credentials");
-        return res.status(400).json({message:"Invalid Credentials"})
-      }
+};
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-      generateTokenandsetCookies(user._id,res);
+    //Checking the user from the database
+    const user = await User.findOne({ username: username });
 
-      res.status(200).json(
-        {
-          _id:user._id,
-          fullName: user.fullName,
-          username: user.username,
-        profilePic:user.profilePic
-      })
-
-      
-    } catch (error) {
-      console.log(` Error in Login controller: ${error}`);
-      res.status(500).json({message:"Internal Server Error"})
-      
+    //Checking that the password matches with the database password or not
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      console.log("Invalid Credentials");
+      return res.status(400).json({ message: "Invalid Credentials" });
     }
-} 
-export const  logout = (req,res)=>{
-    res.send("Logout Route")
-}
 
+    generateTokenandsetCookies(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log(` Error in Login controller: ${error}`);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+      maxAger: 0,
+    });
+    res.status(200).json({ message: "Logged Out Successfully" });
+  } catch (error) {
+    console.log(` Error in Logout controller: ${error}`);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
