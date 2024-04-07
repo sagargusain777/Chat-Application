771578f2486df01f1cd3772be37928e048942e53
uwuxverticuse <sagargusain150@gmail.com>
2024-04-07 23:42:16 +0530

    ✅ Added route for the message

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectDB from "./db/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({limit: "16kb"}));
+//Route For the authentication
 app.use("/api/auth", authRoutes);
+//Route for the Messages
+app.use('/api/messages',messageRoutes)
 connectDB()
   .then(() => {
     {
